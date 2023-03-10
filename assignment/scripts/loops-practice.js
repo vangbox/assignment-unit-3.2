1b41ba84e2711ea5567f04b3f6b19d6f47722e87
Vang Vang <vang2code@gmail.com>
2023-03-10 16:22:44 -0600

    completed question 2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -30,7 +30,9 @@ for (let i=2; i<11; i+=2){
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i=5; i>=0; i--){
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
@@ -38,7 +40,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for( let i=0; i<stars.length; i++){
+  console.log(stars[i]);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
